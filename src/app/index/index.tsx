cd8e7fc64b117714f037deb585a1992971372fea
ALINE APARECIDA ALVES <aline.aalves9@senacsp.edu.br>
2024-05-13 22:09:43 -0300

    primeiro commit

---
+++ src/app/index/index.tsx
@@ -0,0 +1,34 @@
+import {View, Text,ScrollView} from 'react-native';
+import {styles} from './styles';
+import { Ingredient } from '@/components/ingredient';
+
+export default function Home(){
+
+    function handleToggleSelected(){
+        
+    }
+
+
+    return(
+        <View style={styles.container}>
+            <Text style={styles.title}>
+                Escolha {"\n"}
+                <Text style={styles.subtitle}>os produtos</Text>
+            </Text>
+            <Text style={styles.message}>
+                Descubra receitas baseadas nos produtos que você escolheu
+            </Text>
+            {/*<Ingredient/>*/}
+            <ScrollView contentContainerStyle = {styles.ingredients}
+            showsVerticalScrollIndicator={false}
+            >
+                
+             {Array.from({length:100}).map ((item, index)=> (<Ingredient key={index}
+                name="Maça" image="" selected={false} onPress={() => handleToggleSelected}
+             
+             />
+             ))}
+            </ScrollView>
+        </View>
+    )
+}
